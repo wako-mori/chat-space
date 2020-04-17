e7646ac488881ad2323e32069d38086255685fb2
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-04-17 13:39:09 +0900

    updata

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [800, 800]
+  process resize_to_limit: [400, 400]
   # def scale(width, height)
   #   # do something
   # end
@@ -34,9 +34,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
