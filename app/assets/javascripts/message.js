1e62fab461a557b681f9cc6ca42bfcebc084d4ac
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-04-23 14:37:02 +0900

    fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,7 +63,7 @@ $(function(){
     })
   })
   var reloadMessages = function(){
-    if(window.location.href.match(/messages/)){
+    if(window.location.href.match(/\/groups\/\d+\/messages/)){
     var last_message_id = $('.each__disply:last').data("message-id");
     console.log(last_message_id)
     $.ajax({
