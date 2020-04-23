aaf0d710286da13e41ecfccf5fc0c0bdf3da292d
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-04-23 11:16:39 +0900

    Fix image_placement

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -16,7 +16,7 @@ $(function(){
                         ${message.content}
                       </p>
                     </div>
-                    <img src=${message.image}>
+                    <img class="messages__image" src=${message.image}>
                   </div>`
       return html;
     } else {
