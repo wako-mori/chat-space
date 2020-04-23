d205a72d8f28eefbc9a2028cd9f034af3c96fff7
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-04-23 13:00:05 +0900

    戻す

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -64,7 +64,7 @@ $(function(){
   })
   var reloadMessages = function(){
     if(window.location.href.match(/messages/)){
-    var last_message_id = $('.disply:last').data("message-id");
+    var last_message_id = $('.each__disply:last').data("message-id");
     $.ajax({
       url: "api/messages",
       type: 'get',
